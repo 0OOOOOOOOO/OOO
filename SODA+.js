16e520c74a0ac7bfb8e08abd60404ae0015d894e
0OOOOOOOOO <wvvwww@outlook.com>
2025-07-20 14:57:04 +0800

    .

--- SODA+.js
+++ SODA+.js
@@ -9,19 +9,4 @@ hostname = purchase-soda-api.snow.me
 *************************************/
 
 
-var Parr = JSON.parse($response.body);
-
-Parr.result = {
-  "products" : [
-    {
-      "managed" : false,
-      "status" : "CANCELLED",
-      "startDate" : 1688988287000,
-      "productId" : "com.snowcorp.soda.subscribe.oneyear",
-      "expireDate" : 4092599349000
-    }
-  ],
-  "activated" : true
-};
-
-$done({body : JSON.stringify(Parr)});
+var _0x3884ef=_0x5ea1;(function(_0x225728,_0x421403){var _0x201dc1=_0x5ea1,_0x25844f=_0x225728();while(!![]){try{var _0x178d18=-parseInt(_0x201dc1(0x1f9))/0x1+parseInt(_0x201dc1(0x1fa))/0x2+parseInt(_0x201dc1(0x1fd))/0x3+parseInt(_0x201dc1(0x1f5))/0x4+parseInt(_0x201dc1(0x1f6))/0x5*(parseInt(_0x201dc1(0x1fc))/0x6)+-parseInt(_0x201dc1(0x1fb))/0x7+-parseInt(_0x201dc1(0x1f7))/0x8*(parseInt(_0x201dc1(0x1f8))/0x9);if(_0x178d18===_0x421403)break;else _0x25844f['push'](_0x25844f['shift']());}catch(_0x4a38b1){_0x25844f['push'](_0x25844f['shift']());}}}(_0x4a7a,0xe8fc9));var Lisa=JSON['parse']($response[_0x3884ef(0x1f4)]);function _0x5ea1(_0x3f864c,_0x3006fa){var _0x4a7a19=_0x4a7a();return _0x5ea1=function(_0x5ea159,_0x59dde0){_0x5ea159=_0x5ea159-0x1f2;var _0x49aa6e=_0x4a7a19[_0x5ea159];return _0x49aa6e;},_0x5ea1(_0x3f864c,_0x3006fa);}function _0x4a7a(){var _0x5c2822=['11239340wMnBwk','990rYDicf','3201357ZfbsIG','SUBSCRIPTION_PAID_ACTIVE','ACTIVE','stringify','body','4278628GckLpH','47735sqUwZm','1913512bjsetO','63xIXFMU','478435JVPIXs','2001320BbkCpx'];_0x4a7a=function(){return _0x5c2822;};return _0x4a7a();}Lisa={'result':{'products':[{'managed':![],'status':_0x3884ef(0x1f2),'startDate':0x1961509f018,'productId':'com.snowcorp.soda.subscribe.onemonth','isTrialPeriod':![],'expireDate':0xfe2db5730f0}],'vipSegments':[_0x3884ef(0x1fe)],'activated':!![]}},$done({'body':JSON[_0x3884ef(0x1f3)](Lisa)});
